8516b2b2ddbd259b525f9589fc94dcf7c40adb44
kirilltobola <kirilltobola@gmail.com>
2025-07-20 19:11:45 +0800

    Исправлена неработающая ссылка

--- datasets/msls_val/download_moscow.sh
+++ datasets/msls_val/download_moscow.sh
@@ -5,7 +5,10 @@ declare download_link;
 
 # MSLS-Moscow
 file_name="msls_moscow.zip";
-download_link="https://cloud.icc.ru/index.php/s/sWHQK5ies5cDB8d/download/msls_moscow.zip";
+download_link="https://cloud.icc.ru/index.php/s/J6H3wQe55T2jjAj/download/msls_moscow.zip";
+
+mkdir -p train_val;
+cd train_val;
 
 echo Downloading $file_name...;
 wget "$download_link" -O $file_name;
